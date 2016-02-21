b217754cb980c3f09e56ea4af768602d41936973
chenglin <l800891@me.com>
2016-02-21 15:01:19 -0500

    Initial Commit

---
+++ CLTypingLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CLTypingLabel
+//
+//  Created by Lin on 2/21/16.
+//  Copyright © 2016 Chenglin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
